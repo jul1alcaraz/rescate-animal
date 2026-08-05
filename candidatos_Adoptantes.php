24a2c962f5a13010aaf93e5e6e82d2ec1063c956
Julieta Alcaraz Aballay <142432213+jul1alcaraz@users.noreply.github.com>
2026-08-05 20:36:54 -0300

    InifnityFree code adapt

--- candidatos_Adoptantes.php
+++ candidatos_Adoptantes.php
@@ -1,22 +1,23 @@
 <?php
-$nombre_form = $_POST ["Nombre"];
-$apellido_form = $_POST ["Apellido"];
-$Date_form = $_POST ["Date"];
-$Localidad_form = $_POST ["Localidad"];
-$NumeroCelular_form = $_POST ["NumeroCelular"];
-$sugerencias_form = $_POST ["sugerencias"];
+require_once __DIR__ . '/db-config.php';
 
-/*conexion a base de datos. INSERT
-$conexion = mysqli_connect("localhost", "root", "", "rescate_animal") or exit("No se pudo conectar a base de datos");*/
+$nombre_form = $_POST["Nombre"];
+$apellido_form = $_POST["Apellido"];
+$Date_form = $_POST["Date"];
+$Localidad_form = $_POST["Localidad"];
+$NumeroCelular_form = $_POST["NumeroCelular"];
+$sugerencias_form = $_POST["sugerencias"];
 
+$conexion = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME) or exit("No se pudo conectar a base de datos");
 
-$conexion = mysqli_connect("localhost", "id20275451_ulises01", "!R|7rh^EG}~yJJ?)", "id20275451_rescate_animal") or exit("No se pudo conectar a base de datos");
-
-//CONAULTA//
-mysqli_query( $conexion, "INSERT INTO candidatos_adoptantes 
-VALUES (DEFAULT, '$nombre_form', '$apellido_form', '$Date_form', '$Localidad_form', '$NumeroCelular_form', '$sugerencias_form') " );
+$consulta = mysqli_prepare($conexion,
+    "INSERT INTO candidatos_adoptantes
+    VALUES (DEFAULT, ?, ?, ?, ?, ?, ?)");
+mysqli_stmt_bind_param($consulta, "ssssss",
+    $nombre_form, $apellido_form, $Date_form, $Localidad_form, $NumeroCelular_form, $sugerencias_form);
+mysqli_stmt_execute($consulta);
+mysqli_stmt_close($consulta);
 
 mysqli_close($conexion);
 
-header ("Location: comunicate.php?ok");
-
+header("Location: comunicate.php?ok");
